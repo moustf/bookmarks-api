7bcb68d4f195c41ed353d34f1f5929cd3d97c406
Mustafa Salem <mustafaissasalem@gmail.com>
2023-01-07 22:28:50 +0200

    feat: added the built in validation pipe with white space option set to true to allow only the required data to be sent

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // ? Ensures that we don't receive any data we didn't define
+      whitelist: true,
+    }),
+  );
   await app.listen(8888);
 }
 bootstrap();
