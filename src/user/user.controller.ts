9ed9faa92104eee5f7ee07d8d55c8af06082d753
Mustafa Salem <mustafaissasalem@gmail.com>
2023-01-08 22:35:53 +0200

    feat: added the controller for the users routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Body, Get, Patch, UseGuards } from '@nestjs/common';
+import { JwtGuard } from '../auth/guard';
+import { GetUser } from '../auth/decorator';
+import { User } from '@prisma/client';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
+
   @Get('me')
-  getMe() {
-    return {
-      name: 'Mustafa Salem',
-    };
+  getMe(@GetUser('id') user: User) {
+    return user;
+  }
+
+  @Patch()
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
   }
 }
